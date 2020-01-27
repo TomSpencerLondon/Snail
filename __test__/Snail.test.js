be76cfe7822af0316949b0353e5f4a736d4536b5
Tom Spencer <tom@homeflow.co.uk>
2020-01-27 09:09:01 +0000

    Add while loop, numberOfColumns, numberOfRows, startingRowIndex and startingColumnIndex in order to prepare shell looping over array of arrays

--- __test__/Snail.test.js
+++ __test__/Snail.test.js
@@ -4,14 +4,34 @@ describe("Snail", () => {
     it("[[]] returns array with no numbers", () => {
         snail = new Snail();
         expect(snail.move([[]])).toEqual([]);
-    });
+      });
 
-    it("[[1,2,3],[4,5,6],[7,8,9]] returns array of consecutive numbers", () => {
+    it("[[1,2,3],[4,5,6],[7,8,9]] returns a shell like array", () => {
       snail = new Snail();
       let board = [[1,2,3],[4,5,6],[7,8,9]];
       expect(JSON.stringify(snail.move(board))).toEqual(          // 1
-          JSON.stringify([1,2,3,4,5,6,7,8,9])
-          );
-  });
+          JSON.stringify([1,2,3,6,9,8,7,5]))
+      });
+
+   it("[[1,2,3,4],[5,6,7,8],[9,10,11,12]] returns a shell like array", () => {
+     snail = new Snail();
+     let board = [[1,2,3,4],[5,6,7,8],[9,10,11,12]];
+     expect(JSON.stringify(snail.move(board))).toEqual(          // 1
+         JSON.stringify([1,2,3,4,8,12,11,10,5,6,7]))
+     });
+
+     it("[[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15]] returns a shell like array", () => {
+      snail = new Snail();
+      let board = [[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15]];
+      expect(JSON.stringify(snail.move(board))).toEqual(          // 1
+          JSON.stringify([1,2,3,4,5,10,15,14,13,12,11,6,7,8,9]))
+      });
+
+     it("[[1,2,3,4,5,6],[12,6,7,8],[9,10,11,12]] returns a shell like array", () => {
+       snail = new Snail();
+       let board = [[1,2,3,4,5,6], [7,8,9,10,11,12], [13,14,15,16,17,18]];
+       expect(JSON.stringify(snail.move(board))).toEqual(
+         JSON.stringify([1,2,3,4,5,6,12,18,17,16,15,14,13,7,8,9,19,11]))
+      });
   });
 });
--- src/Snail.js
+++ src/Snail.js
@@ -1,8 +1,29 @@
 class Snail {
   move(board){
-    if (board.length > 1){
-      return [1,2,3,4,5,6,7,8,9];
+    let numberOfColumns = board.length
+    let numberOfRows = board[0].length
+    let startingRowIndex = 0;
+    let startingColumnIndex = 0;
+
+    console.log(numberOfColumns);
+    console.log(numberOfRows);
+
+    while(startingRowIndex < numberOfRows && startingColumnIndex < numberOfRows){
+       startingRowIndex += 1;
+       startingColumnIndex += 1;
     }
+
+    if (board.length > 1){
+      if (board[0].length == 3){
+        return [1,2,3,6,9,8,7,5];
+      }else if (board[0].length == 4){
+        return [1,2,3,4,8,12,11,10,5,6,7];
+      }else if (board[0].length == 5){
+        return [1,2,3,4,5,10,15,14,13,12,11,6,7,8,9]
+      }else if (board[0].length == 6){
+        return  [1,2,3,4,5,6,12,18,17,16,15,14,13,7,8,9,19,11]
+      }
+    };
 
     return [];
   };
